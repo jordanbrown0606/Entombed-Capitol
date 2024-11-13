350ece0feec3891ec2fb329fb63559853d6b3bae
jordanbrown0606 <157506730+jordanbrown0606@users.noreply.github.com>
2024-11-13 13:47:50 -0600

    Updating scene transitions.

--- Entombed Captiol/Assets/Scripts/Enemy&Player Scipts/HagglerScript.cs
+++ Entombed Captiol/Assets/Scripts/Enemy&Player Scipts/HagglerScript.cs
@@ -14,19 +14,6 @@ public class HagglerScript : MonoBehaviour, IDamageable
     public Transform trueBox;
 
     public GameObject gateKey;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void MoveFalse()
     {
         gateKey.transform.position = falseBox.position;
