4dd2ae8b39d03f66011cbf91b791454ba61953ee
JtMecm <mcooperblack@gmail.com>
2024-11-20 12:48:51 -0600

    Lighthouse Work
    Lighthouse can be ascended and descended, light can be lit now

--- Entombed Captiol/Assets/Scripts/StairTeleport.cs
+++ Entombed Captiol/Assets/Scripts/StairTeleport.cs
@@ -1,27 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StairTeleport : MonoBehaviour
 {
-    public GameObject stairLower;
-    public GameObject stairUpper;
+    public GameObject player;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    private void OnTriggerEnter()
-    {
-        if (stairLower)
+        if (this.transform.position.y < 10)
         {
             Debug.Log("You touched the lower stairs!");
+            player.transform.position = new Vector3(-1.6f, 28.34f, -4.34f);
         }
-        if (stairUpper)
+        if (this.transform.position.y > 10)
         {
             Debug.Log("You touched the upper stairs!");
+            player.transform.position = new Vector3(2.39f, 1.1f, 5.56f);
         }
     }
 }
